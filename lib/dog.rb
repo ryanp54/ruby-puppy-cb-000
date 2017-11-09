adb1d0dc568c007fe18586c54918f3f796ca4474
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 03:29:49 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
